92f8f4c56ed7e5cef8af9ea429915a457d802498
Yupei-Wang <62461705+Yupei-Wang@users.noreply.github.com>
2023-02-24 17:26:24 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-batch=50
+batch=200
 group=$1
 unseen_classes=oos
-seed=2022
+seed=2020
 dataset=CLINC_OOD_full
 train_class_num=n
 detect_method="msp"
@@ -13,7 +13,7 @@ if [ $group = 0 ];then
 
 # test
 elif [ $group = 1 ];then
-    model_dir="./output/CLINC_OOD_full-1_oos-n-2022-None-ce200/"
+    model_dir="./output/CLINC_OOD_full-1_oos-n-2020-None-ce200/"
 
     python main.py --dataset $dataset --mode test --model_dir $model_dir --detect_method $detect_method  --unseen_classes $unseen_classes --seed $seed --batch_size $batch --cuda --train_class_num $train_class_num
 
@@ -25,10 +25,10 @@ elif [ $group = 2 ];then
 
 # Bayes
 elif [ $group = 3 ];then
-    model_dir="./output/CLINC_OOD_full-1_oos-n-2022-None-ce200/"
+    model_dir="./output/CLINC_OOD_full-1_oos-n-2020-None-ce200/"
     mc_dropout=True
     mc_pro=0.7
-    mc_round=100
+    mc_round=150
     dataset='CLINC_OOD_full'
 
     python main.py --mode test --model_dir $model_dir --mc_dropout --mc_round $mc_round --mc_pro $mc_pro --dataset $dataset --unseen_classes $unseen_classes --detect_method $detect_method --seed $seed --batch_size $batch --cuda --train_class_num $train_class_num
